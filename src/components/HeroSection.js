e58b6281c008f43b8982da427cbb4125680b7001
Densingh-123 <ddensingh19@gmail.com>
2025-09-28 22:29:15 +0530

    updated fhir name

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -80,7 +80,7 @@ const HeroSection = () => {
               color: 'white'
             }}
           >
-            FEHR
+            FEIR
           </motion.div>
           <div className="flow-arrow">→</div>
           <motion.div
